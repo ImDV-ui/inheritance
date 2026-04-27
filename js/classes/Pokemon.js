d01146084730faccd1c9c0b021f9543ab3039cb6
ImDV-ui <daviddv2512@gmail.com>
2026-04-27 08:49:20 +0200

    update

--- js/classes/Pokemon.js
+++ js/classes/Pokemon.js
@@ -1,23 +1,37 @@
 import TypeChart from '../game/TypeChart.js';
-import Entity from './Entity.js';
 
-export default class Pokemon extends Entity {
+export default class Pokemon {
     constructor({ name, level, types, baseStats, moves, spriteFront, spriteBack }) {
-        const maxHealth = Math.floor((2 * baseStats.hp * level) / 100) + level + 10;
-        super(name, maxHealth);
+        this.name = name;
         this.level = level;
         this.types = types;
         this.stats = baseStats;
         this.moves = moves;
         this.spriteFront = spriteFront;
         this.spriteBack = spriteBack;
+
+
+        this.maxHealth = Math.floor((2 * this.stats.hp * this.level) / 100) + this.level + 10;
+        this.health = this.maxHealth;
+        this.isAlive = true;
+
         this.attackStat = Math.floor((2 * this.stats.attack * this.level) / 100) + 5;
         this.defenseStat = Math.floor((2 * this.stats.defense * this.level) / 100) + 5;
         this.speedStat = Math.floor((2 * this.stats.speed * this.level) / 100) + 5;
     }
 
+    takeDamage(amount) {
+        if (!this.isAlive) return;
+        this.health -= amount;
+        if (this.health <= 0) {
+            this.health = 0;
+            this.isAlive = false;
+        }
+    }
+
     calculateDamage(move, defender) {
         if (move.power <= 0) return { amount: 0, effectiveness: 1 };
+
 
         const levelFactor = (2 * this.level) / 5 + 2;
         const statFactor = this.attackStat / defender.defenseStat;
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import PokeAPI from './api/PokeAPI.js';
-import GameController from './game/GameController.js';
+import BattleManager from './game/BattleManager.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
     const logElem = document.getElementById('log-text');
@@ -7,19 +7,19 @@ document.addEventListener('DOMContentLoaded', async () => {
     
     logElem.innerText = "Conectando con la Dex de Kanto (PokeAPI)...";
     
+
     const pId = Math.floor(Math.random() * 151) + 1;
     let eId = Math.floor(Math.random() * 151) + 1;
     if (eId === pId) eId = (eId % 151) + 1;
 
     try {
         logElem.innerText = `Descargando información en paralelo de IDs ${pId} y ${eId}...`;
-        
         const [playerPokemon, enemyPokemon] = await Promise.all([
             PokeAPI.getPokemon(pId, 50),
             PokeAPI.getPokemon(eId, 50)
         ]);
 
-        new GameController(playerPokemon, enemyPokemon);
+        new BattleManager(playerPokemon, enemyPokemon);
 
     } catch (e) {
         logElem.innerText = "Hubo un error cargando los datos de la PokéAPI. Revisa la consola.";
